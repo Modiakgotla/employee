93bbb1500b82ee282e0a9b8143f80d0e1f3b8771
Modiakgotla <rethamagonare@hotmail.com>
2022-06-23 16:01:17 +0200

    employee

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import '../src/components/css/App.css';
+
+import AddEmployee from './components/AddEmployee'
+function App() {
+  return (
+    <div className="App">
+<AddEmployee/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AddEmployee.js
@@ -0,0 +1,20 @@
+
+import "../src/components/css/AppEmployee.css";
+
+function AddEmployee(){
+return(
+<div>
+<input placeholder="First Name" /><br></br>
+
+<input placeholder="Last Name" /><br></br>
+
+<input placeholder="Email" /><br></br>
+
+
+<button id="btn">Add Employee</button>
+</div>
+
+)
+;}
+
+export default AddEmployee;
